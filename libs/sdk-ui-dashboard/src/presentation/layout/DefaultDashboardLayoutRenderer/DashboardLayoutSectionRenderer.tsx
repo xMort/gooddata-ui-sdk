0f908a1942312fb94c6337a572aa8385a2000d11
xMort <petr.dolejsi@gooddata.com>
2024-10-01 11:39:04 +0200

    feat: styleable sections in css grid

--- libs/sdk-ui-dashboard/src/presentation/layout/DefaultDashboardLayoutRenderer/DashboardLayoutSectionRenderer.tsx
+++ libs/sdk-ui-dashboard/src/presentation/layout/DefaultDashboardLayoutRenderer/DashboardLayoutSectionRenderer.tsx
@@ -1,16 +1,21 @@
 // (C) 2007-2024 GoodData Corporation
 import React from "react";
 import { IDashboardLayoutSectionRenderer } from "./interfaces.js";
+import { implicitLayoutItemSizeFromXlSize } from "./utils/sizing.js";
+import { GRID_COLUMNS_COUNT } from "./constants.js";
 // import cx from "classnames";
 
 // const isHiddenStyle = { height: 0, width: 0, overflow: "hidden", flex: 0 };
 // const defaultStyle = {};
 
 export const DashboardLayoutSectionRenderer: IDashboardLayoutSectionRenderer<unknown> = (props) => {
-    const { children /*, className, debug, isHidden*/ } = props;
+    const { children, section, screen /*, className, debug, isHidden*/ } = props;
 
     // const style = isHidden ? isHiddenStyle : defaultStyle;
-    // console.log("XXXX DashboardLayoutSectionRenderer")
+    console.log("XXXX DashboardLayoutSectionRenderer");
+
+    const possibleLayoutSizes = implicitLayoutItemSizeFromXlSize(section.raw().size!);
+    const layoutSize = possibleLayoutSizes[screen];
 
     // TODO move the functionality to widget itself, no wrapper here to fuck up the grid
     return (
@@ -20,7 +25,13 @@ export const DashboardLayoutSectionRenderer: IDashboardLayoutSectionRenderer<unk
         //     })}
         //     style={style}
         // >
-        <>{children}</>
+        <div
+            className={`gd-grid-layout__item--section gd-grid-layout__item--span-${
+                layoutSize?.gridWidth ?? GRID_COLUMNS_COUNT
+            }`}
+        >
+            {children}
+        </div>
         // </div>
     );
 };
